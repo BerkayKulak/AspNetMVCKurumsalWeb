fe080737e822c65094828944fed66c6d265e4a66
Berkay Kulak <kulakberkay15@gmail.com>
2021-07-09 13:47:52 +0300

    Add project files.

---
+++ AspNetKurumsalWeb/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetKurumsalWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetKurumsalWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        KurumsalDBEntities db = new KurumsalDBEntities();
+
+        public ActionResult Index()
+        {
+            var sorgu = db.Kategoris.ToList();
+            return View(sorgu);
+        }
+    }
+}
