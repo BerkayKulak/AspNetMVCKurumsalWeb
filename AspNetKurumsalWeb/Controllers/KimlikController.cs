2288a84ffcdf558904102fbeb0c045ada6a31322
Berkay Kulak <kulakberkay15@gmail.com>
2021-07-09 23:49:36 +0300

    9/07

---
+++ AspNetKurumsalWeb/Controllers/KimlikController.cs
@@ -0,0 +1,92 @@
+﻿using AspNetKurumsalWeb.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetKurumsalWeb.Controllers
+{
+    public class KimlikController : Controller
+    {
+        KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Kimlik
+        public ActionResult Index()
+        {
+            return View(db.Kimlik.ToList());
+        }
+
+        //// GET: Kimlik/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Kimlik/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Kimlik/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: Kimlik/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var kimlik = db.Kimlik.Where(x => x.KimlikId == id).SingleOrDefault();
+            return View(kimlik);
+        }
+
+        // POST: Kimlik/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //// GET: Kimlik/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Kimlik/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
