5646ecf06c92811d3d67f91e183686073274a16b
Berkay Kulak <kulakberkay15@gmail.com>
2021-07-10 20:45:02 +0300

    10/07

--- AspNetKurumsalWeb/Controllers/HizmetController.cs
+++ AspNetKurumsalWeb/Controllers/HizmetController.cs
@@ -2,6 +2,7 @@
 using AspNetKurumsalWeb.Models.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,7 @@ namespace AspNetKurumsalWeb.Controllers
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Create(Hizmet hizmet,HttpPostedFileBase ResimURL)
         {
             if(ModelState.IsValid)
@@ -36,11 +38,11 @@ namespace AspNetKurumsalWeb.Controllers
                     //{
                     //    System.IO.File.Delete(Server.MapPath(kimlik.LogoURL));
                     //}
-
+                
                     WebImage img = new WebImage(ResimURL.InputStream);
                     FileInfo imginfo = new FileInfo(ResimURL.FileName);
 
-                    string logoname = ResimURL.FileName + imginfo.Extension;
+                    string logoname = Guid.NewGuid().ToString() + imginfo.Extension;
                     img.Resize(500, 500);
                     img.Save("~/Uploads/Hizmet/" + logoname);
 
@@ -54,6 +56,75 @@ namespace AspNetKurumsalWeb.Controllers
             return View(hizmet);
         }
 
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                ViewBag.Uyari = "Güncellenecek Hizmet Bulunamadı";
+            }
+            var hizmet = db.Hizmet.Find(id);
+            if(hizmet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hizmet);
+        }
+
+
+        // name'inden yakaladığı için ResimURl yaptık.
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(int? id, Hizmet hizmet, HttpPostedFileBase ResimURl)
+        {
+          
+            if(ModelState.IsValid)
+            {
+                var h = db.Hizmet.Where(x => x.HizmetId == id).SingleOrDefault();
+                if (ResimURl != null)
+                {
+                    //resim varsa silecek
+                    if (System.IO.File.Exists(Server.MapPath(h.ResimURL)))
+                    {
+                        System.IO.File.Delete(Server.MapPath(h.ResimURL));
+                    }
+                    // resim yoksa resim yükleme işlemlerini yapacak
+
+                    WebImage img = new WebImage(ResimURl.InputStream);
+                    FileInfo imginfo = new FileInfo(ResimURl.FileName);
+
+                    string hizmetName = Guid.NewGuid().ToString() + imginfo.Extension;
+                    img.Resize(500, 500);
+                    img.Save("~/Uploads/Hizmet/" + hizmetName);
+
+                    h.ResimURL = "/Uploads/Hizmet/" + hizmetName;
+                }
+
+                h.Baslik = hizmet.Baslik;
+                h.Aciklama = hizmet.Aciklama;
+                db.SaveChanges();
+                // mevcut kontrollerin indexine dön
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if(id==null)
+            {
+                return HttpNotFound();
+            }
+            var h = db.Hizmet.Find(id);
+            if(h==null)
+            {
+                return HttpNotFound();
+            }
+            db.Hizmet.Remove(h);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
 
     }
 
