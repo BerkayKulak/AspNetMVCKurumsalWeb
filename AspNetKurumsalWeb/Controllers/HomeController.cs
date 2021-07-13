829c7aab3eb18e5a7996e3ed6c737ab091ede2a4
Berkay Kulak <kulakberkay15@gmail.com>
2021-07-13 14:05:43 +0300

    13/07

--- AspNetKurumsalWeb/Controllers/HomeController.cs
+++ AspNetKurumsalWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetKurumsalWeb.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,32 @@ namespace AspNetKurumsalWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private KurumsalDBContext db = new KurumsalDBContext();
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x => x.HizmetId);
+
+            ViewBag.Iletisim = db.Iletisim.SingleOrDefault();
+
+            ViewBag.Blog = db.Blog.ToList().OrderByDescending(x => x.BlogId);
+                
             return View();
+        }
+
+        public ActionResult SliderPartial()
+        {
+            return View(db.Slider.ToList().OrderByDescending(x=>x.SliderId));
+        }
+
+        public ActionResult HizmetPartial()
+        {
+            return View(db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId));
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View(db.Hakkimizda.SingleOrDefault());
         }
     }
 }
